22e0f9e15f75e54bd7d7be47e16b2b3851f90f08
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2024-05-05 02:09:06 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,13 @@
+# login into azure account
+from azure.identity import DefaultAzureCredential
+credentail = DefaultAzureCredential()
+
+# provide your subsciption id
+subscription_id = 
+from azure.mgmt.resource import ResourceManagementClient
+resource_client = ResourceManagementCLient(credentail , subscription_id)
+
+# create Resource Group with name and location 
+rg_result = resource_client.resource_group.create_or_update(
+    "chayandeokar" , { "location" : "westus2" }
+)
